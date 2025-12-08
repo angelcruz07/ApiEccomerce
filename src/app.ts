4279c9af653e7930b06d50fac13364d632709f64
angelcruz07 <angelcruzsanchez126@gmail.com>
2025-12-07 19:31:15 -0600

    feat: add CORS support with specific options and update dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,22 @@
 import allRoutes from "@routes/index.routes";
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors"
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
+// O con opciones más específicas:
+app.use(cors({
+    origin: '*', // Permite todos los orígenes (en producción usa el dominio específico)
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use("/api", allRoutes);
 
 export default app;
